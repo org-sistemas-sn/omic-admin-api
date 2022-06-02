1047538bce0b93c8ecd1aa760156d7be7d4ae14a
Franco <dev.francosilva@gmail.com>
2022-06-02 13:26:04 -0300

    fojas entities

--- src/fojas/fojas.module.ts
+++ src/fojas/fojas.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+import { Denuncia } from 'src/denuncias/entities/denuncia.entity';
+import { Foja } from './entities/foja.entity';
+import { Archivo } from './entities/archivo.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Denuncia, Foja, Archivo])],
+})
 export class FojasModule {}
